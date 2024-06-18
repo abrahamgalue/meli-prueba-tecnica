dcb68021812bf90c4a6d9022e5b7b79c10f63937
Abraham Galue <abrahamgalue512@gmail.com>
2024-06-18 13:11:32 -0400

    Added image resource

---
+++ src/app/images.js
@@ -0,0 +1,11 @@
+export const IMAGES = [
+  {
+    url: 'https://http2.mlstatic.com/D_NQ_792140-MLA76970349287_062024-OO.webp'
+  },
+  {
+    url: 'https://http2.mlstatic.com/D_NQ_886249-MLA76939315609_062024-OO.webp'
+  },
+  {
+    url: 'https://http2.mlstatic.com/D_NQ_967875-MLA75595473640_042024-OO.webp'
+  }
+]
