75eaf297713290f7a058807385ffc5b152cc0754
Abraham Galue <abrahamgalue512@gmail.com>
2024-06-19 09:33:04 -0400

    Improved carousel accessibility

--- src/app/components/ImagesSlider.tsx
+++ src/app/components/ImagesSlider.tsx
@@ -5,30 +5,34 @@ import { useState } from 'react'
 import { ArrowIcon } from '../icons/Icons'
 
 interface Props {
-  imgUrls: {
+  images: {
     url: string
+    alt: string
   }[]
 }
 
-export function ImagesSlider({ imgUrls }: Props) {
+export function ImagesSlider({ images }: Props) {
   const [imageIndex, setImageIndex] = useState(0)
 
   const showNextImage = () => {
     setImageIndex(index => {
-      if (index === imgUrls.length - 1) return 0
+      if (index === images.length - 1) return 0
       return index + 1
     })
   }
 
   const showPrevImage = () => {
     setImageIndex(index => {
-      if (index === 0) return imgUrls.length - 1
+      if (index === 0) return images.length - 1
       return index - 1
     })
   }
 
   return (
-    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+    <section
+      aria-label='Image Carousel'
+      style={{ width: '100%', height: '100%', position: 'relative' }}
+    >
       <div
         style={{
           width: '100%',
@@ -37,25 +41,34 @@ export function ImagesSlider({ imgUrls }: Props) {
           overflow: 'hidden',
         }}
       >
-        {imgUrls.map((img, index) => (
+        {images.map(({ url, alt }, index) => (
           <img
-            key={img.url}
-            src={img.url}
+            key={url}
+            src={url}
+            alt={alt}
+            aria-hidden={imageIndex !== index}
             className='slider-img'
             width={1920}
             height={505}
             style={{
               translate: `${-100 * imageIndex}%`,
             }}
-            alt={`Photo ${index + 1}`}
           />
         ))}
       </div>
-      <button onClick={showPrevImage} className='img-slider-btn previous'>
-        <ArrowIcon />
+      <button
+        onClick={showPrevImage}
+        className='img-slider-btn previous'
+        aria-label='View Previous Image'
+      >
+        <ArrowIcon aria-hidden />
       </button>
-      <button onClick={showNextImage} className='img-slider-btn next'>
-        <ArrowIcon />
+      <button
+        onClick={showNextImage}
+        className='img-slider-btn next'
+        aria-label='View Next Image'
+      >
+        <ArrowIcon aria-hidden />
       </button>
       <div
         style={{
@@ -64,21 +77,24 @@ export function ImagesSlider({ imgUrls }: Props) {
           left: '50%',
           translate: '-50%',
           display: 'flex',
-          gap: '.25rem',
+          gap: '.1rem',
         }}
       >
-        {imgUrls.map((_, index) => (
+        {images.map((_, index) => (
           <button
             key={index}
             className={`img-slider-dot-btn ${
               index === imageIndex ? 'active' : null
             }`}
             onClick={() => setImageIndex(index)}
+            aria-label={`View Image ${index + 1}`}
           >
-            <span className='img-slider-dot-btn-span'>Página {index}</span>
+            <span className='img-slider-dot-btn-span' aria-hidden={true}>
+              Página {index}
+            </span>
           </button>
         ))}
       </div>
-    </div>
+    </section>
   )
 }
